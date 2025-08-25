179267b41f418ea4fbe151f785c8d740e6e2e470
Chetan7528 <kaklotarchetan7528@gmail.com>
2025-08-25 11:47:03 +0530

    minor change

--- src/app/controller/admin.js
+++ src/app/controller/admin.js
@@ -1000,6 +1000,7 @@ module.exports = {
             };
           }
           gaurds[a.username]._id = a._id;
+          gaurds[a.username].payroll = a?.payroll?.type || 'weekly'
         }
       }
 
